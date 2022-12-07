5d4072fc659c405058fd643cdcb2702971aaa7a1
aldo lopez <aldo.lopez@houserx.com>
2022-12-07 13:48:20 -0500

    tokens

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { MoovDrops } from './moov';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const token = await axios.get('/.netlify/functions/moveToken');
+        console.log(token);
+        setToken(token.data);
+      } catch (e) {
+        console.log('an error occurred');
+        console.log(e);
+      }
+    };
+    getToken();
+  }, []);
+
+  if (!token) {
+    return <div>loading ...</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MoovDrops
+        accountId={token.accountId}
+        onCancel={() => alert('canceled')}
+        onSuccess={() => alert('success')}
+        token={token.token}
+      />
     </div>
   );
 }
