ec95a7cdf1e09ccba293e836dc23822e24ec0d79
aldo lopez <aldo.lopez@houserx.com>
2023-01-06 15:46:21 -0500

    camera test

--- src/App.js
+++ src/App.js
@@ -1,45 +1,88 @@
+import React from 'react';
+import Webcam from "react-webcam";
+
 import './App.css';
 import { MoovDrops } from './moov';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+const WebcamCapture = () => {
+  const [images, setImages] = useState([]);
+
+  return (
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      >
+        {({ getScreenshot }) => (
+          <button
+            onClick={() => {
+              const imageSrc = getScreenshot();
+              setImages([...images, imageSrc]);
+            }}
+          >
+            Capture photo
+          </button>
+        )}
+      </Webcam>
+      {images.map((b64) => {
+        return (
+          <div><img src={b64} /></div>
+        );
+      })}
+    </>
+  )
+};
+
 function App() {
-  const [token, setToken] = useState(null);
-  const [addingPayment, setAddingPayment] = useState(false);
+  // const [token, setToken] = useState(null);
+  // const [addingPayment, setAddingPayment] = useState(false);
 
-  const add = () => {
-    console.log('adding');
-    setAddingPayment(true);
-  };
+  // const add = () => {
+  //   console.log('adding');
+  //   setAddingPayment(true);
+  // };
 
-  useEffect(() => {
-    const getToken = async () => {
-      try {
-        const token = await axios.get('/.netlify/functions/moovToken');
-        console.log(token);
-        setToken(token.data);
-      } catch (e) {
-        console.log('an error occurred');
-        console.log(e);
-      }
-    };
-    getToken();
-  }, []);
+  // useEffect(() => {
+  //   const getToken = async () => {
+  //     try {
+  //       const token = await axios.get('/.netlify/functions/moovToken');
+  //       console.log(token);
+  //       setToken(token.data);
+  //     } catch (e) {
+  //       console.log('an error occurred');
+  //       console.log(e);
+  //     }
+  //   };
+  //   getToken();
+  // }, []);
 
-  if (!token) {
-    return <div>loading ...</div>
-  }
+  // if (!token) {
+  //   return <div>loading ...</div>
+  // }
   return (
     <div className="App">
-      <button onClick={add}>add payment</button>
-      {addingPayment && (
-        <MoovDrops
-          accountId={token.accountId}
-          onCancel={() => setAddingPayment(false)}
-          onSuccess={() => setAddingPayment(false)}
-          token={token.token}
-        />
-      )}
+      {/* <button onClick={add}>add payment</button>
+      {addingPayment && ( */
+        // <MoovDrops
+        //   accountId={token.accountId}
+        //   onCancel={() => setAddingPayment(false)}
+        //   onSuccess={() => setAddingPayment(false)}
+        //   token={token.token}
+        // />
+        // )}
+      }
+      <WebcamCapture />
     </div>
   );
 }
